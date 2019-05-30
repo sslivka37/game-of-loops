df79a2b98d1217008c7e352a3167f36c43096006
sslivka <sslivka@DESKTOP-QHJ85C1>
2019-05-30 08:03:26 -0400

    first commit, laid out the skeleton of the project and created some menu methods, need to build out the actual games

---
+++ LoopsGame/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoopsGame
+{
+    class Menu
+    {
+        public int MenuInput { get; set; }
+        public string PlayAgainInput { get; set; }
+
+        public void DisplayMenu()
+        {
+            Console.WriteLine("Please choose a game to play:");
+            Console.WriteLine("Press 1 to play 'Count the As in a Name' game.");
+            Console.WriteLine("Press 2 to play 'Max and Min Values' game.");
+            Console.WriteLine("Press 3 to play 'Count by Odds' game.");
+            Console.WriteLine("Press 4 to play 'Guess the Binary PIN' game.");
+            Console.WriteLine("Press 5 to play 'Backwards Name' game.");
+            Console.WriteLine("Press 6 to play 'Number Summation' game");
+            Console.WriteLine("Press 7 to play 'Username Creation' game.");
+            Console.WriteLine("Press 8 to Exit");
+
+            //press 8 to view high score? change exit to 9
+        }
+
+        public void AskToPlayAgain()
+        {
+            Console.WriteLine("Would you like to play again? If so, type 'yes'");
+                
+        }
+
+        public void CheckPlayAgain()
+        {
+            if (PlayAgainInput.Equals("yes", StringComparison.InvariantCultureIgnoreCase))
+            {
+                DisplayMenu();
+            }
+            else 
+            {
+                Console.WriteLine("Thank you for playing!");
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ LoopsGame/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LoopsGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the game of loops!");
+
+            //pick a game (user input)
+
+            //1-counting the 'a's game
+
+            //2-max and min values game
+
+            //3-count by odds game
+
+            //4-guess the binary PIN game
+
+            //5-name backwards (string helper?)
+
+            //6-summation game using <List> -> look for List summation method?
+
+            //7-create a valid username that is 4 characters long and has one letter, digit, and special character !*?#
+
+
+
+
+            //play again?
+
+            //high score with name -> to .txt file
+
+
+
+        }
+    }
+}
